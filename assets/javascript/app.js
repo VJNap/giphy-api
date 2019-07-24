d65ed9b2eac2f50785bcdc91e1bad2d0ad3e7ce9
VJNap <vjnapolitano1364@gmail.com>
2019-07-23 23:40:13 -0500

    VN - Adjusted array name errors in displayGifs function to allow static gifs to be displayed based on the static queryURL.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,6 +1,6 @@
 $(document).ready(function () {
 
-    var gifTopics = ["Puppies", "Cars", "World of Warcraft", "Food", "Coding"];
+    var gifTopics = ["Puppies", "Cars", "World of Warcraft", "Food", "Coding", "Cigars"];
     // Function to display buttons
     function displayButtons() {
         $("#topicButtons").empty();
@@ -12,37 +12,44 @@ $(document).ready(function () {
             $("#topicButtons").append(topicButton);
         }
     };
+
+    // Missing add new button function
+        // Take input from the search field and add a new button to gifTopics
+
     // Function to display gifs
     function displayGifs() {
 
         var gifTopic = $(this).attr("data-name");
-        // "Cars" to be replaced with working variable once I can console.log a response
-        var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=UCBcB9fbjGC7LhVaEC3bdwRFmz0AAFfN&q=" + "Cars" + "&limit=10&offset=0&rating=PG&lang=en";
+        // "Cars" to be replaced with working variable once I can establish the correct path
+        var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=UCBcB9fbjGC7LhVaEC3bdwRFmz0AAFfN&q=" + "Cigars" + "&limit=10&offset=0&rating=PG-13&lang=en";
         $.ajax({
             url: queryURL,
             method: "GET"
         })
             .then(function (response) {
                 console.log(response);
-                // $("#topicGifs").empty();
-                // var gifs = response.data
-                // for (var i = 0; i < gifs.length; i++) {
-                //     var gifDiv = $("<div>");
-                //     gifDiv.addClass("gifDiv");
-                //     var gifRating = $("<p>").text("Rating: " + results[i].rating);
-                //     gifDiv.append(gifRating);
-                //     var gifImage = $("<img>");
-                //     gifImage.attr("src", results[i].images.fixed_height_small_still.url);
-                //     gifImage.attr("data-still", results[i].images.fixed_height_small_still.url);
-                //     gifImage.attr("data-animate", results[i].images.fixed_height_small.url);
-                //     gifImage.attr("data-state", "still");
-                //     gifDiv.append(gifImage);
-                //     $("#topicGifs").prepend(gifDiv);
-                // }
+                $("#topicGifs").empty();
+                var gifs = response.data
+                for (var i = 0; i < gifs.length; i++) {
+                    var gifDiv = $("<div>");
+                    gifDiv.addClass("gifDiv");
+                    var gifRating = $("<p>").text("Rating: " + gifs[i].rating);
+                    gifDiv.append(gifRating);
+                    var gifImage = $("<img>");
+                    gifImage.attr("src", gifs[i].images.fixed_height_small_still.url);
+                    gifImage.attr("data-still", gifs[i].images.fixed_height_small_still.url);
+                    gifImage.attr("data-animate", gifs[i].images.fixed_height_small.url);
+                    gifImage.attr("data-state", "still");
+                    gifDiv.append(gifImage);
+                    $("#topicGifs").prepend(gifDiv);
+                }
             });
 
     }
+    // Missing on click functionality for buttons and animating gifs
     displayButtons();
+    // addButtons();
+    displayGifs();
     //end of document ready
 });
 
